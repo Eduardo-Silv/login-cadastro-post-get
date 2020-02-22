a960eb2585b5c642fe78aaf7f129881d7f8004a2
Eduardo Silva <eduardo9631s@gmail.com>
2020-02-21 21:56:57 -0300

    Criação de arquivo de login e cadastro para demonstração de métodos HTTP GET e POST

---
+++ cadastrar.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8"/>
+<?php 
+    $nomeDoCadastrado = $_POST["nome"];
+    $cpfDoCadastrado = $_POST["cpf"];
+    $idadeDoCadastrado = $_POST["idade"];
+
+    echo "$nomeDoCadastrado, você foi Cadastrado com Sucesso !!!";
+?>
